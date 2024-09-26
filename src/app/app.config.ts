39b399034c7a9cfdd179e8ef245f16f97fff398c
Xuan Nguyen Thanh <thanhxuanhd007@gmail.com>
2024-09-26 22:43:01 +0700

    Fixed bug and url use hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import { provideHighlightOptions } from 'ngx-highlightjs';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
     provideHighlightOptions({
       fullLibraryLoader: () => import('highlight.js'),
       lineNumbersLoader: () => import('ngx-highlightjs/line-numbers')
-    })
+    }), {
+      provide: LocationStrategy, useClass: HashLocationStrategy
+    }
   ]
 };
